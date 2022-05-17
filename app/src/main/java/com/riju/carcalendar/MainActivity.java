e2383a38fa1bc8c3d42c760e2b947b723e9497cd
GRiju <49880783+GRiju@users.noreply.github.com>
2022-05-17 08:39:31 +0200

    Action bar removed, saves data in json file

--- app/src/main/java/com/riju/carcalendar/MainActivity.java
+++ app/src/main/java/com/riju/carcalendar/MainActivity.java
@@ -14,6 +14,7 @@ import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.CalendarContract;
 import android.util.Log;
 import android.view.View;
@@ -27,7 +28,17 @@ import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.Task;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
@@ -35,19 +46,24 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.ProtocolException;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.sql.Time;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    long startMillis = 0;
+    long endMillis = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ActivityCompat.requestPermissions(MainActivity.this, new String[]{ Manifest.permission.WRITE_CALENDAR, Manifest.permission.READ_CALENDAR}, 1);
+        ActivityCompat.requestPermissions(MainActivity.this, new String[]{ Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
 //        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
 //
 //        Intent signInIntent = GoogleSignIn.getClient(this, gso).getSignInIntent();
@@ -55,117 +71,14 @@ public class MainActivity extends AppCompatActivity {
 //
 //        startActivityForResult(signInIntent, 1);
 
-        Button startbtn = (Button)this.findViewById(R.id.startbtn);
-        TextView starttime = (TextView)this.findViewById(R.id.starttime);
+        TextView starttime = (TextView) this.findViewById(R.id.starttime);
 
-        startbtn.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
-                Calendar time = Calendar.getInstance();
-                time.setTime(new Date());
-                time.set(Calendar.HOUR_OF_DAY, time.get(Calendar.HOUR_OF_DAY) + 1);
+        Calendar time = Calendar.getInstance();
+        time.setTimeInMillis(Long.parseLong(getCarDataJson("starttime")));
 
-            }
-        });
-
-        int permCalendar = ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_CALENDAR);
-
-        if(permCalendar != PackageManager.PERMISSION_GRANTED)
-        {
-            ActivityCompat.requestPermissions(MainActivity.this, new String[]{ Manifest.permission.WRITE_CALENDAR, Manifest.permission.READ_CALENDAR}, 1);
-
-        }
-        if(permCalendar == PackageManager.PERMISSION_GRANTED) {
-            long calID = getCalendarId();
-
-            if (calID == -1) {
-                Toast.makeText(MainActivity.this, "Naptár nem található", Toast.LENGTH_LONG).show();
-                return;
-            }
-
-            long startMillis = 0;
-            long endMillis = 0;
-            Calendar time = Calendar.getInstance();
-            time.setTime(new Date());
-            startMillis = time.getTimeInMillis();
-            time.set(Calendar.HOUR_OF_DAY, time.get(Calendar.HOUR_OF_DAY) + 1);
-
-            endMillis = time.getTimeInMillis();
-
-            ContentResolver cr = getContentResolver();
-            ContentValues values = new ContentValues();
-            values.put(CalendarContract.Events.DTSTART, startMillis);
-            values.put(CalendarContract.Events.DTEND, endMillis);
-            values.put(CalendarContract.Events.TITLE, "Event Example");
-            values.put(CalendarContract.Events.DESCRIPTION, "Description to the example event");
-            values.put(CalendarContract.Events.CALENDAR_ID, calID);
-            values.put(CalendarContract.Events.EVENT_TIMEZONE, "America/Los_Angeles");
-            values.put(CalendarContract.Events.EVENT_LOCATION, "New York City");
-
-            //Uri uri = cr.insert(CalendarContract.Events.CONTENT_URI, values);
-
-
-//_-------------------
-            String[] projection = new String[] { CalendarContract.Calendars._ID, CalendarContract.Calendars.ACCOUNT_NAME, CalendarContract.Calendars.CALENDAR_DISPLAY_NAME };
-            String selection = CalendarContract.Calendars.ACCOUNT_NAME + " = ? AND "
-                    + CalendarContract.Calendars.ACCOUNT_TYPE + " = ? ";
-            // use the same values as above:
-            //String[] selArgs = new String[] { calendarName, CalendarContract.ACCOUNT_TYPE_LOCAL  };
-            String[] selArgs = new String[] { "oszvaldgergo20@gmail.com", "com.google"  };
-
-            Cursor cursor = getContentResolver().query(CalendarContract.Calendars.CONTENT_URI, projection, selection,
-                    selArgs, null);
-
-            String out = "";
-            while (cursor.moveToNext()) {
-
-                out += cursor.getLong(0) + ", " + cursor.getString(1) + ", "  + cursor.getString(2) + "\r\n";
-            }
-
-            AlertDialog.Builder dlgAlert  = new AlertDialog.Builder(this);
-            dlgAlert.setMessage(out);
-            dlgAlert.setTitle("App Title");
-            dlgAlert.setPositiveButton("OK", null);
-            dlgAlert.setCancelable(true);
-            dlgAlert.create().show();
-        }
-
-//        URL url = null;
-//        try {
-//            url = new URL("https://www.googleapis.com/calendar/v3/calendars/[CALENDARID]/events?key=[YOUR_API_KEY] HTTP/1.1");
-//        } catch (MalformedURLException e) {
-//            e.printStackTrace();
-//        }
-//
-//        HttpURLConnection con = null;
-//        try {
-//            con = (HttpURLConnection)url.openConnection();
-//            con.setRequestMethod("POST");
-//
-//            con.setRequestProperty("Content-Type", "application/json; utf-8");
-//            con.setRequestProperty("Accept", "application/json");
-//
-//            con.setDoOutput(true);
-//
-//            String jsonInputString = "{key: value, key: value";
-//
-//            try(OutputStream os = con.getOutputStream()) {
-//                byte[] input = jsonInputString.getBytes("utf-8");
-//                os.write(input, 0, input.length);
-//            }
-//
-//            try(BufferedReader br = new BufferedReader(
-//                    new InputStreamReader(con.getInputStream(), "utf-8"))) {
-//                StringBuilder response = new StringBuilder();
-//                String responseLine = null;
-//                while ((responseLine = br.readLine()) != null) {
-//                    response.append(responseLine.trim());
-//                }
-//                System.out.println(response.toString());
-//            }
-//
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy.MM.dd HH:mm:ss");
+        String datestring = sdf.format(time.getTime());
+        starttime.setText(datestring);
     }
 
     private long getCalendarId() {
@@ -175,7 +88,11 @@ public class MainActivity extends AppCompatActivity {
                 + CalendarContract.Calendars.CALENDAR_DISPLAY_NAME + " = ? ";
         // use the same values as above:
         //String[] selArgs = new String[] { calendarName, CalendarContract.ACCOUNT_TYPE_LOCAL  };
-        String[] selArgs = new String[] { "oszvaldgergo20@gmail.com", "com.google", "Jövőbeli esegfbmények" };
+        //String[] selArgs = new String[] { "oszvaldgergo20@gmail.com", "com.google", "Jövőbeli események" };
+
+        String calendarname = ((TextView)findViewById(R.id.calendarname)).getText().toString();
+
+        String[] selArgs = new String[] { "oszvaldgergo20@gmail.com", "com.google", calendarname };
         if (ActivityCompat.checkSelfPermission(this,
                 Manifest.permission.READ_CALENDAR) != PackageManager.PERMISSION_GRANTED) {
             return -1;
@@ -213,5 +130,163 @@ public class MainActivity extends AppCompatActivity {
             Log.w("Hiba", "signInResult:failed code=" + e.getStatusCode());
             //updateUI(null);
         }
+    }
+
+    public void AddStartTime(View v)
+    {
+        TextView starttime = (TextView)this.findViewById(R.id.starttime);
+
+        Calendar time = Calendar.getInstance();
+        time.setTime(new Date());
+
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy.MM.dd HH:mm:ss");
+        String datestring = sdf.format(new Date());
+        starttime.setText(datestring);
+
+        startMillis = time.getTimeInMillis();
+
+        JSONObject jsonObject = new JSONObject();
+        try {
+            jsonObject.put("starttime", startMillis);
+
+            String userString = jsonObject.toString();
+// Define the File Path and its Name
+            File file = new File(getFilesDir(), "cardata.json");
+            FileWriter fileWriter = new FileWriter(file);
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+            bufferedWriter.write(userString);
+            bufferedWriter.close();
+        } catch (JSONException | IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String getCarDataJson(String attribute)
+    {
+        File file = new File(getFilesDir(), "cardata.json");
+        FileReader fileReader = null;
+        try {
+            fileReader = new FileReader(file);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            StringBuilder stringBuilder = new StringBuilder();
+            String line = bufferedReader.readLine();
+            while (line != null){
+                stringBuilder.append(line).append("\n");
+                line = bufferedReader.readLine();
+            }
+            bufferedReader.close();
+
+            String response = stringBuilder.toString();
+
+            JSONObject jsonObject  = new JSONObject(response);
+            return jsonObject.get(attribute).toString();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return "";
+    }
+
+    public void AddEndTime(View v)
+    {
+        File file = new File(getFilesDir(), "cardata.json");
+        FileReader fileReader = null;
+        try {
+            fileReader = new FileReader(file);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            StringBuilder stringBuilder = new StringBuilder();
+            String line = bufferedReader.readLine();
+            while (line != null){
+                stringBuilder.append(line).append("\n");
+                line = bufferedReader.readLine();
+            }
+            bufferedReader.close();
+
+            String response = stringBuilder.toString();
+
+            JSONObject jsonObject  = new JSONObject(response);
+            jsonObject.get("starttime").toString();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        TextView endtime = (TextView)this.findViewById(R.id.endtime);
+        Calendar time = Calendar.getInstance();
+        time.setTime(new Date());
+
+        endMillis = time.getTimeInMillis();
+
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy.MM.dd HH:mm:ss");
+        String datestring = sdf.format(new Date());
+        endtime.setText(datestring);
+    }
+
+    public void AddEventToCalendar(View v)
+    {
+        int permCalendar = ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_CALENDAR);
+
+//                if(permCalendar != PackageManager.PERMISSION_GRANTED)
+//                {
+//                    ActivityCompat.requestPermissions(MainActivity.this, new String[]{ Manifest.permission.WRITE_CALENDAR, Manifest.permission.READ_CALENDAR}, 1);
+//
+//                }
+        if(permCalendar == PackageManager.PERMISSION_GRANTED) {
+            long calID = getCalendarId();
+
+            if (calID == -1) {
+                Toast.makeText(MainActivity.this, "Naptár nem található", Toast.LENGTH_LONG).show();
+                return;
+            }
+
+
+            ContentResolver cr = getContentResolver();
+            ContentValues values = new ContentValues();
+            values.put(CalendarContract.Events.DTSTART, startMillis);
+            values.put(CalendarContract.Events.DTEND, endMillis);
+            values.put(CalendarContract.Events.TITLE, "Autó használat");
+            //values.put(CalendarContract.Events.DESCRIPTION, "Description to the example event");
+            values.put(CalendarContract.Events.EVENT_TIMEZONE, Calendar.getInstance().getTimeZone().toString());
+            values.put(CalendarContract.Events.CALENDAR_ID, calID);
+
+            Uri uri = cr.insert(CalendarContract.Events.CONTENT_URI, values);
+        }
+    }
+
+    public void ListMyCalendars()
+    {
+        String[] projection = new String[] { CalendarContract.Calendars._ID, CalendarContract.Calendars.ACCOUNT_NAME, CalendarContract.Calendars.CALENDAR_DISPLAY_NAME };
+        String selection = CalendarContract.Calendars.ACCOUNT_NAME + " = ? AND "
+                + CalendarContract.Calendars.ACCOUNT_TYPE + " = ? ";
+        // use the same values as above:
+        //String[] selArgs = new String[] { calendarName, CalendarContract.ACCOUNT_TYPE_LOCAL  };
+        String[] selArgs = new String[] { "oszvaldgergo20@gmail.com", "com.google"  };
+
+        Cursor cursor = getContentResolver().query(CalendarContract.Calendars.CONTENT_URI, projection, selection,
+                selArgs, null);
+
+        String out = "";
+        while (cursor.moveToNext()) {
+
+            out += cursor.getLong(0) + ", " + cursor.getString(1) + ", "  + cursor.getString(2) + "\r\n";
+        }
+
+        AlertDialog.Builder dlgAlert  = new AlertDialog.Builder(this);
+        dlgAlert.setMessage(out);
+        dlgAlert.setTitle("App Title");
+        dlgAlert.setPositiveButton("OK", null);
+        dlgAlert.setCancelable(true);
+        dlgAlert.create().show();
+    }
+
+    public void SaveConfig(View view)
+    {
     }
 }
